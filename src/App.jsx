cdb49fd1d9e225b6a2ca5cbcacdebe0d5083c888
andrealar30 <dreaguilar098@gmail.com>
2023-05-10 12:52:05 -0600

    Incremento y decremento

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { SocialNetworks } from "./components/socialNetworks/SocialNetworks";
 import { NavBar } from "./components/navBar/NavBar";
 import { Footer } from "./components/footer/Footer";
+import { Counter } from "./components/counter/Counter";
 
 export const App = () => {
   return (
     <>
       <NavBar />
       <SocialNetworks />
+      <Counter initialValue={100} increment={5} decrement={5} /> 
+      <Counter initialValue={0} increment={10}/> 
       <Footer />
     </>
   );
---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { Button } from "../counter/Button.jsx";
+
+
+export const Counter = ( {initialValue=0, increment=1, decrement=1} ) => {
+   
+    const [counter, setcounter] = useState( initialValue );
+ 
+    const handleIncrement = (event)=>{       
+        setcounter( counter + increment )        
+    }
+ 
+    const handleDecrement = (event)=>{       
+        setcounter( counter - decrement )        
+    }
+ 
+    const handleReset = (event)=>{       
+        setcounter( initialValue ) ;       
+    }
+
+
+  return (
+     <>
+        <h1>Counter</h1>        
+        <h2>{ counter }</h2>
+        <Button onClick= { handleIncrement  } >+</Button>
+        <Button onClick= { handleDecrement  } >-</Button>
+        <Button onClick= { handleReset  } >Reset</Button>
+
+     </>   
+  );
+};
+
+/*
+ Hacer un componente para el botòn.
+ atributo: increment={5}
+ atributo: increment={-5}
+ atributo: onClick={ fncCallBack }
+ Color text: blue;
+
+*/
